f3014f34394701145b7a3e84ff4db4d89ee80cd2
bochaga-masaval <33434341+bochaga-masaval@users.noreply.github.com>
2017-12-21 10:10:35 -0300

    Update index.js for ES6
    Solve Problem minifying with ES6

--- index.js
+++ index.js
@@ -1,43 +1,30 @@
-'use strict'
-
-function clean (rut) {
-  return typeof rut === 'string'
-    ? rut.replace(/^0+|[^0-9kK]+/g, '').toUpperCase()
-    : ''
+"use strict";
+function clean(a) {
+  return "string" == typeof a
+    ? a.replace(/^0+|[^0-9kK]+/g, "").toUpperCase()
+    : "";
 }
 
-function validate (rut) {
-  if (typeof rut !== 'string') {
-    return false
-  }
-  if (!/^0*(\d{1,3}(\.?\d{3})*)-?([\dkK])$/.test(rut)) {
-    return false
-  }
-
-  rut = clean(rut)
-
-  let t = parseInt(rut.slice(0, -1), 10)
-  let m = 0
-  let s = 1
-
-  while (t > 0) {
-    s = (s + t % 10 * (9 - m++ % 6)) % 11
-    t = Math.floor(t / 10)
-  }
-
-  const v = s > 0 ? `${s - 1}` : 'K'
-  return v === rut.slice(-1)
+function validate(a) {
+  if ("string" != typeof a) return !1;
+  if (!/^0*(\d{1,3}(\.?\d{3})*)-?([\dkK])$/.test(a)) return !1;
+  a = clean(a);
+  for (var b = parseInt(a.slice(0, -1), 10), c = 0, d = 1; 0 < b; )
+    (d = (d + (b % 10) * (9 - c++ % 6)) % 11), (b = Math.floor(b / 10));
+  var e = 0 < d ? "" + (d - 1) : "K";
+  return e === a.slice(-1);
 }
 
-function format (rut) {
-  rut = clean(rut)
-
-  let result = `${rut.slice(-4, -1)}-${rut.substr(rut.length - 1)}`
-  for (let i = 4; i < rut.length; i += 3) {
-    result = rut.slice(-3 - i, -i) + '.' + result
-  }
-
-  return result
+function format(a) {
+  a = clean(a);
+  for (
+    var b = a.slice(-4, -1) + "-" + a.substr(a.length - 1), c = 4;
+    c < a.length;
+    c += 3
+  )
+    b = a.slice(-3 - c, -c) + "." + b;
+  return b;
 }
 
-module.exports = { validate, clean, format }
+module.exports = { validate: validate, clean: clean, format: format };
+
