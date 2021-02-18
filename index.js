780b9f4001d1c6ac47486f54994dfd6aea6af300
Jesús Lobos <jlobos@outlook.cl>
2021-02-18 09:21:27 -0300

    refactoring of `getDigit`

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict'
-
 function clean (rut) {
   return typeof rut === 'string'
     ? rut.replace(/^0+|[^0-9kK]+/g, '').toUpperCase()
@@ -16,49 +14,56 @@ function validate (rut) {
 
   rut = clean(rut)
 
-  var t = parseInt(rut.slice(0, -1), 10)
-  var m = 0
-  var s = 1
+  let t = parseInt(rut.slice(0, -1), 10)
+  let m = 0
+  let s = 1
 
   while (t > 0) {
-    s = (s + (t % 10) * (9 - m++ % 6)) % 11
+    s = (s + (t % 10) * (9 - (m++ % 6))) % 11
     t = Math.floor(t / 10)
   }
 
-  var v = s > 0 ? '' + (s - 1) : 'K'
+  const v = s > 0 ? '' + (s - 1) : 'K'
   return v === rut.slice(-1)
 }
 
 function format (rut) {
   rut = clean(rut)
 
-  var result = rut.slice(-4, -1) + '-' + rut.substr(rut.length - 1)
-  for (var i = 4; i < rut.length; i += 3) {
+  let result = rut.slice(-4, -1) + '-' + rut.substr(rut.length - 1)
+  for (let i = 4; i < rut.length; i += 3) {
     result = rut.slice(-3 - i, -i) + '.' + result
   }
 
   return result
 }
 
-function getDigit (rut) {
-  rut = rut+"";
-  // type check
-  if (!rut || !rut.length || typeof rut !== 'string') {
-    return -1;
+function getCheckDigit (input) {
+  const rut = Array.from(clean(input), Number)
+
+  if (rut.length === 0 || rut.includes(NaN)) {
+    throw new Error(`"${input}" as RUT is invalid`)
   }
-  // serie numerica
-  var secuencia = [2,3,4,5,6,7,2,3];
-  var sum = 0;
-  //
-  for (var i=rut.length - 1; i >=0; i--) {
-      var d = rut.charAt(i)
-      sum += new Number(d)*secuencia[rut.length - (i + 1)];
-  };
-  // sum mod 11
-  var rest = 11 - (sum % 11);
-  // si es 11, retorna 0, sino si es 10 retorna K,
-  // en caso contrario retorna el numero
-  return rest === 11 ? 0 : rest === 10 ? "K" : rest;
+
+  const modulus = 11
+  const initialValue = 0
+  const sumResult = rut
+    .reverse()
+    .reduce(
+      (accumulator, currentValue, index) =>
+        accumulator + currentValue * ((index % 6) + 2),
+      initialValue
+    )
+
+  const checkDigit = modulus - (sumResult % modulus)
+
+  if (checkDigit === 10) {
+    return 'K'
+  } else if (checkDigit === 11) {
+    return '0'
+  } else {
+    return checkDigit.toString()
+  }
 }
 
-module.exports = { validate: validate, clean: clean, format: format, getDigit:getDigit}
+module.exports = { validate, clean, format, getCheckDigit }
